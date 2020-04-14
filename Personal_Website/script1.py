cb276a7c0d0455ec6276b5605df9d2c9ee0d492a
Harsh <18harshgandhi@gmail.com>
2020-04-14 20:53:22 +0530

    Add files via upload
    https://basic1234.herokuapp.com/

---
+++ Personal_Website/script1.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 11 18:53:59 2020
+
+@author: Harsh
+"""
+
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+@app.route('/about/')
+
+def about():
+    return render_template("about.html")
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=False)
+
